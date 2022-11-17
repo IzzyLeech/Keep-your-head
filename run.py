e3a4dba82c4f453a788bc5e44ba63b8d2cbedd0a
IzzyLeech <ralphhunter46@gmail.com>
2022-11-17 12:59:09 +0000

    add validation to username for letter only

--- run.py
+++ run.py
@@ -4,6 +4,18 @@ from nltk.corpus import words
 wordlist = words.words()
 random_word = random.choice(wordlist)
 
+
+def main():
+    validate_username()
+
+
+def validate_username():
+    while True:
+        username = input("Please enter you name:")
+        if username.isalpha():
+            break
+        print("INVALID NAME, please try again")
+
+
 print("Welcome to keep your head")
-username = input("Please enter you name:")
-print(username)
+main()
