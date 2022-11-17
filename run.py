39d75ca59ff0e28edc2f5f8d78dc146b914a3c76
IzzyLeech <ralphhunter46@gmail.com>
2022-11-17 13:15:36 +0000

    add docstrings to functions

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 import random
 from nltk.corpus import words
-
 wordlist = words.words()
 random_word = random.choice(wordlist)
 
 
 def main():
+    """
+    Main that runs all function
+    """
     validate_username()
 
 
 def validate_username():
+    """
+    Validates the username to ensure that it contain only letter,
+    so that numbers or whitespace can't be enter.
+    """
     while True:
         username = input("Please enter you name:")
         if username.isalpha():
