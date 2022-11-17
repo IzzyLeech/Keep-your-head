ac4f8736004b5a34796aced537adf69930a81629
IzzyLeech <ralphhunter46@gmail.com>
2022-11-17 12:13:44 +0000

    add welcome text and a input for username

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from nltk.corpus import words
 
 wordlist = words.words()
 random_word = random.choice(wordlist)
-print(random_word)
+
+print("Welcome to keep your head")
+username = input("Please enter you name:")
+print(username)
